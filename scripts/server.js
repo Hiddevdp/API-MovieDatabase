9f011b0cbc96b82d0d7d427a0052e3b77683789d
Hiddevdp <hiddevanderplaat@live.nl>
2024-05-07 12:00:11 +0200

    update

--- scripts/server.js
+++ scripts/server.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 
 dotenv.config();
 
-app.use("/styles", serveStatic(join(__dirname, "../styles")));
+app.use("/styles", serveStatic(join(__dirname, "../styles"))); //code om de static files te serven
 app.use("/scripts", serveStatic(join(__dirname, "../scripts")));
 
 const apiKey = process.env.API_KEY;
@@ -26,12 +26,12 @@ app.get("/", async (req, res) => {
   console.log("Route handler for '/' is being called");
   console.log("Rendering index");
   try {
-    const all = await fetchDataForType("all");
-    res.render("index", { movies: all });
+    const all = await fetchDataForType("all"); //haal met functie de array met films/series op
+    res.render("index", { movies: all }); //laad de ejs in met de array
   } catch (error) {
     console.log("An error occurred while trying to render index.ejs:", error);
   }
-});
+}); // route voor de homepagina, laadt standaard "all" in
 
 app.get("/movies", async (req, res) => {
   try {
@@ -41,7 +41,7 @@ app.get("/movies", async (req, res) => {
     console.error("An error occurred while fetching movies:", error);
     res.status(500).send("An error occurred while fetching movies.");
   }
-});
+}); // route voor movies pagina
 
 app.get("/series", async (req, res) => {
   try {
@@ -51,7 +51,7 @@ app.get("/series", async (req, res) => {
     console.error("An error occurred while fetching series:", error);
     res.status(500).send("An error occurred while fetching series.");
   }
-});
+}); //route voor series pagina
 
 app.get("/all", async (req, res) => {
   try {
@@ -61,25 +61,27 @@ app.get("/all", async (req, res) => {
     console.error("An error occurred while fetching all:", error);
     res.status(500).send("An error occurred while fetching all.");
   }
-});
+}); //route voor all pagina
 
 app.get("/info/:type/:id", async (req, res) => {
   const id = req.params.id;
   const type = req.params.type;
-  const details = await fetchDetailsForId(type, id);
+  const details = await fetchDetailsForId(type, id); //haal de details van specifieke film/serie op
   console.log(details);
   res.render("details", { details: details });
-});
+}); //route voor info van specifieke film/serie
 
 async function fetchDataForType(type) {
+  //functie voor het ophalen van array van films/series/alles
   try {
     const movies = [];
     for (let page = 1; page <= 3; page++) {
+      //voor het inladen van meer dan een pagina
       let url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}&page=${page}`;
       if (type === "series") {
         url = `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}&page=${page}`;
       } else if (type === "movies") {
-        url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&page=${page}`;
+        url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&page=${page}`;
       } else if (type === "all") {
         url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&page=${page}`;
       }
@@ -107,26 +109,5 @@ async function fetchDetailsForId(type, id) {
   const details = await response.json();
   return details;
 }
-
-// async function fetchMovieData(itemId, mediaType) {
-//   const url = `https://api.themoviedb.org/3/${mediaType}/${itemId}?api_key=${apiKey}`;
-//   console.log(`Fetching data for ${mediaType} with ID ${itemId}`);
-//   console.log(`Fetching data from URL: ${url}`);
-//   const response = await fetch(url);
-//   console.log(response);
-//   if (!response.ok) {
-//     throw new Error(`HTTP error! status: ${response.status}`);
-//   }
-//   const itemData = await response.json();
-//   return itemData;
-// }
-
-// function getMovieData(movieId, mediaType, movies) {
-//   const movie = movies.find(
-//     (movie) =>
-//       String(movie.id) === movieId && String(movie.media_type) === mediaType
-//   );
-//   return movie;
-// }
 
 app.listen(3000, () => console.log("Started on http://localhost:3000"));
