22a265728b8ebf039cbc14d04fd43592cc4572bb
Hiddevdp <hiddevanderplaat@live.nl>
2024-05-07 03:26:30 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,58 @@
-document.getElementById("movies").addEventListener("click", function () {
-  fetchData("movies");
-});
+// document.getElementById("movies").addEventListener("click", function () {
+//   fetchData("movies");
+// });
 
-document.getElementById("series").addEventListener("click", function () {
-  fetchData("series");
-});
+// document.getElementById("series").addEventListener("click", function () {
+//   fetchData("series");
+// });
 
-document.getElementById("all").addEventListener("click", function () {
-  fetchData("all");
-});
+// document.getElementById("all").addEventListener("click", function () {
+//   fetchData("all");
+// });
 
-function fetchData(type) {
-  fetch(`/${type}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const list = document.getElementById("list");
-      // Clear the list before appending new items
-      list.innerHTML = "";
-      data.forEach((item) => {
-        const card = document.createElement("div");
-        card.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${item.poster_path})`;
-        list.appendChild(card);
-      });
-    })
-    .catch((error) => console.error("Error:", error));
-}
+// function fetchData(type) {
+//   return new Promise((resolve, reject) => {
+//     fetch(`/${type}`)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         const list = document.getElementById("list");
+//         list.innerHTML = "";
+//         data.forEach((item) => {
+//           const card = document.createElement("div");
+//           card.classList.add("card");
+//           card.dataset.id = item.id;
+//           card.dataset.media_type = item.media_type;
+//           card.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${item.poster_path})`;
+//           list.appendChild(card);
+//         });
+//         addEventListeners();
+//         resolve();
+//       })
+//       .catch(reject);
+//   });
+// }
 
-// Fetch all data when the page loads
-fetchData("all");
+// // Add event listeners to the .card elements
+// function addEventListeners() {
+//   document.querySelectorAll(".card");
+//   const cardElements = document.querySelectorAll(".card");
+//   console.log(cardElements.length);
+//   cardElements.forEach((itemElement) => {
+//     itemElement.addEventListener("click", () => {
+//       // Get the ID and media type of the clicked item
+//       const itemId = itemElement.dataset.id;
+//       const mediaType = itemElement.dataset.media_type;
+//       // Navigate to the details page of the clicked item
+//       window.location.href = `/${mediaType}/${itemId}`;
+//       console.log(mediaType, itemId);
+//     });
+//   });
+// }
+
+// // Fetch all data when the page loads
+// fetchData("all");
--- scripts/server.js
+++ scripts/server.js
@@ -1,49 +1,80 @@
 import { App } from "@tinyhttp/app";
 import serveStatic from "serve-static";
-import fetch from "node-fetch";
 import dotenv from "dotenv";
 import { join } from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import ejs from "ejs";
 
-dotenv.config();
+const app = new App();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = new App();
+app.engine(".ejs", ejs.__express);
+app.set("views", join(__dirname, "../views"));
+app.set("view engine", "ejs");
+
+dotenv.config();
+
 app.use("/styles", serveStatic(join(__dirname, "../styles")));
 app.use("/scripts", serveStatic(join(__dirname, "../scripts")));
 
 const apiKey = process.env.API_KEY;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   console.log("Route handler for '/' is being called");
-  console.log("searching for index");
+  console.log("Rendering index");
   try {
-    res.sendFile(join(__dirname, "../index.html"));
-    console.log("index.html has been sent");
+    const all = await fetchDataForType("all");
+    res.render("index", { movies: all });
   } catch (error) {
-    console.log("An error occurred while trying to send index.html:", error);
+    console.log("An error occurred while trying to render index.ejs:", error);
   }
 });
 
 app.get("/movies", async (req, res) => {
-  fetchDataForType("movies", res);
+  try {
+    const movies = await fetchDataForType("movies");
+    res.render("movies", { movies: movies });
+  } catch (error) {
+    console.error("An error occurred while fetching movies:", error);
+    res.status(500).send("An error occurred while fetching movies.");
+  }
 });
 
 app.get("/series", async (req, res) => {
-  fetchDataForType("series", res);
+  try {
+    const series = await fetchDataForType("series");
+    res.render("series", { series: series });
+  } catch (error) {
+    console.error("An error occurred while fetching series:", error);
+    res.status(500).send("An error occurred while fetching series.");
+  }
 });
 
 app.get("/all", async (req, res) => {
-  fetchDataForType("all", res);
+  try {
+    const all = await fetchDataForType("all");
+    res.render("all", { movies: all });
+  } catch (error) {
+    console.error("An error occurred while fetching all:", error);
+    res.status(500).send("An error occurred while fetching all.");
+  }
 });
 
-async function fetchDataForType(type, res) {
+app.get("/info/:type/:id", async (req, res) => {
+  const id = req.params.id;
+  const type = req.params.type;
+  const details = await fetchDetailsForId(type, id);
+  console.log(details);
+  res.render("details", { details: details });
+});
+
+async function fetchDataForType(type) {
   try {
     const movies = [];
-    for (let page = 1; page <= 5; page++) {
+    for (let page = 1; page <= 3; page++) {
       let url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}&page=${page}`;
       if (type === "series") {
         url = `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}&page=${page}`;
@@ -59,11 +90,43 @@ async function fetchDataForType(type, res) {
       const data = await response.json();
       movies.push(...data.results);
     }
-    res.json(movies);
+    return movies;
   } catch (error) {
     console.log("An error occurred while trying to fetch data:", error);
-    res.status(500).send("An error occurred while trying to fetch data.");
   }
 }
 
-app.listen(3000, () => console.log("Started on http://localhost:3000/home"));
+async function fetchDetailsForId(type, id) {
+  const url = `https://api.themoviedb.org/3/${type}/${id}?api_key=${apiKey}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const details = await response.json();
+  return details;
+}
+
+// async function fetchMovieData(itemId, mediaType) {
+//   const url = `https://api.themoviedb.org/3/${mediaType}/${itemId}?api_key=${apiKey}`;
+//   console.log(`Fetching data for ${mediaType} with ID ${itemId}`);
+//   console.log(`Fetching data from URL: ${url}`);
+//   const response = await fetch(url);
+//   console.log(response);
+//   if (!response.ok) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   }
+//   const itemData = await response.json();
+//   return itemData;
+// }
+
+// function getMovieData(movieId, mediaType, movies) {
+//   const movie = movies.find(
+//     (movie) =>
+//       String(movie.id) === movieId && String(movie.media_type) === mediaType
+//   );
+//   return movie;
+// }
+
+app.listen(3000, () => console.log("Started on http://localhost:3000"));
